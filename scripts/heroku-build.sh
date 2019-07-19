37c19a747d605b54463a0ad52c8b5a03fad8921f
Jirat Ki <saakyz@gmail.com>
2019-07-19 16:04:49 +0800

    Support Yarn workspace and deploy 2 apps (#3)
    * Add file
    
    * Move static.json to root
    
    * chmod build-script
    
    * Add engine
    
    * Use npm
    
    * Add yarn.lock
    
    * Switch to yarn workspace
    
    * Create demo-app
    
    * Edit static.json
    
    * Add public
    
    * Uncomment build code
    
    * Fix routes
    
    * Fix root
    
    * fix path
    
    * Fix mv path
    
    * Fix routes
    
    * Remove wildcard
    
    * Remove routes
    
    * Add https and auth

---
+++ scripts/heroku-build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -e
+
+usage() {
+  echo "OVERVIEW: Build apps according to BUILD_ENV value. Meant to be used for Heroku deployment"
+  exit
+}
+
+if [ "$1" = '-h' ] || [ "$1" = '--help' ]; then
+  usage
+fi
+
+(
+  PROJECT_ROOT="$(cd $(dirname $0)/..; pwd)"
+
+  cd $PROJECT_ROOT
+
+  yarn workspace ui build-storybook
+  yarn workspace demo-app build
+
+  mv packages/ui/storybook-static public_html/storybook-static
+  mv packages/demo-app/build public_html/demo-app/
+)
